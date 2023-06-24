4c19408e0457ad3dc306eebc168b8cf622393f97
Shravni Wakde <wakdeshravni@gmail.com>
2023-06-24 16:01:46 +0530

    hosted

--- src/components/Friend.jsx
+++ src/components/Friend.jsx
@@ -23,7 +23,7 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
 
   const patchFriend = async () => {
     const response = await fetch(
-      `http://localhost:6001/users/${_id}/${friendId}`,
+      `https://buzzbox-api.onrender.com/users/${_id}/${friendId}`,
       {
         method: "PATCH",
         headers: {
